1eedceaf3ead8d4c8209b644d5f645828cd9ba0d
claire-labry <clairestjlabry@gmail.com>
2020-06-04 15:59:50 -0500

    added more styling and functionality of the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,105 @@
 import React, { Component } from 'react';
 import PlantCard from './components/PlantCard';
-import Wrapper from './components/Wrapper';
+// import Wrapper from './components/Wrapper';
 import Scoreboard from './components/Scoreboard';
 import plants from './plants.json';
+
+function mix(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 class App extends Component {
   state = {
     plants,
+    score: 0,
+    topScore: 0,
+    displayAlert: 0,
+    showSuccess: 0,
+    clickPhoto: [],
+  };
+
+  clickPhoto = (id) => {
+    let clickPhoto = this.state.clickPhoto;
+    let score = this.state.score;
+    let topScore = this.state.topScore;
+    this.setState({
+      displayAlert: 0,
+    });
+
+    if (clickPhoto.indexOf(id) === -1) {
+      clickPhoto.push(id);
+      console.log(clickPhoto);
+
+      this.handleIncrement();
+      this.createMix();
+    } else if (this.state.score === 12) {
+      this.setState({
+        showSuccess: 1,
+        score: 0,
+        clickPhotos: [],
+      });
+    } else {
+      this.setState({
+        score: 0,
+        clickPhoto: [],
+      });
+      console.log('duplicate');
+      this.setState({
+        showAlert: 1,
+      });
+    }
+
+    if (score > topScore) {
+      this.setState({
+        topScore: score,
+      });
+    }
+  };
+
+  handleIncrement = () => {
+    this.setState({ score: this.state.score + 1 });
+  };
+
+  createMix = () => {
+    this.setState({ plants: mix(plants) });
   };
 
   render() {
     return (
-      <Wrapper>
-        <Scoreboard> Plant Clicky Game</Scoreboard>
-        {this.state.plants.map((plant) => (
-          <PlantCard
-            id={plant.id}
-            key={plant.id}
-            name={plant.name}
-            image={plant.image}
-          />
-        ))}
-      </Wrapper>
+      <div className='container'>
+        <div
+          className='alert alert-danger'
+          style={{ opacity: this.state.showAlert }}
+        >
+          You clicked this plant already, try again!
+        </div>
+        <div
+          className='alert alert-success'
+          style={{ opacity: this.state.showSuccess }}
+        >
+          Look at you! You remember your plants!
+        </div>
+        <Scoreboard
+          title='Plant Clicky Game'
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
+
+        <div className='row'>
+          {this.state.plants.map((plant) => (
+            <PlantCard
+              id={plant.id}
+              key={plant.id}
+              name={plant.name}
+              image={plant.image}
+            />
+          ))}
+        </div>
+      </div>
     );
   }
 }
--- src/components/Scoreboard/index.js
+++ src/components/Scoreboard/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './scoreboard.css';
 
 function Scoreboard(props) {
-  return <h1 className='scoreboard'>{props.children}</h1>;
+  return (
+    <header className='scoreboard'>
+      <div className='row'>
+        <div className='col-md-6 col-left'>
+          <h3>{props.title}</h3>
+        </div>
+        <div className='col-md-3 col-right'>
+          <h4>High Score: {props.topScore}</h4>
+        </div>
+        <div className='col-md-3 col-right'>
+          <h4> Current Score: {props.score}</h4>
+        </div>
+        <div className='row'>
+          <div className='col-md-12 col-right'>
+            <h4>
+              * Test your flower memory! Don't click on a flower more than once.
+              *
+            </h4>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Scoreboard;
