c943db1837d5b7ffb5c6f45dec4757c1974b687a
claire-labry <clairestjlabry@gmail.com>
2020-06-23 19:08:54 -0500

    fixed layout issues in the scoreboard

--- src/components/Scoreboard/index.js
+++ src/components/Scoreboard/index.js
@@ -5,22 +5,21 @@ function Scoreboard(props) {
   return (
     <header className='scoreboard'>
       <div className='row'>
-        <div className='col-md-5 col-left'>
+        <div className=' col-left'>
           <h3>{props.title}</h3>
         </div>
-        <div className='col-md-3 col-right'>
+        <div className='col-right'>
           <h4>High Score: {props.topScore}</h4>
         </div>
-        <div className='col-md-3 col-right'>
+        <div className='col-right'>
           <h4> Current Score: {props.score}</h4>
         </div>
-        <div className='row'>
-          <div className='col-md-12 col-right'>
-            <h4>
-              * Test your flower memory! Don't click on a flower more than once.
-              *
-            </h4>
-          </div>
+      </div>
+      <div className='row'>
+        <div className='col-right'>
+          <h4>
+            * Test your flower memory! Don't click on a flower more than once. *
+          </h4>
         </div>
       </div>
     </header>
