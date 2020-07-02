7e2e84a9714fad8524b2a20c31167d8ba7198451
claire-labry <clairestjlabry@gmail.com>
2020-07-02 11:19:44 -0500

    Updates

---
+++ precache-manifest.df6a0fd0b4d7312099a09cc0f950de62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2c96db47e591510ef85f9a3caf5b30",
+    "url": "/plant-clicky-game/index.html"
+  },
+  {
+    "revision": "990c2fd3885b2541390e",
+    "url": "/plant-clicky-game/static/css/main.7f1ad6bd.chunk.css"
+  },
+  {
+    "revision": "134d5ae0ba6f4a074dec",
+    "url": "/plant-clicky-game/static/js/2.451acaf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plant-clicky-game/static/js/2.451acaf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990c2fd3885b2541390e",
+    "url": "/plant-clicky-game/static/js/main.555411d0.chunk.js"
+  },
+  {
+    "revision": "f45e083479adcf781999",
+    "url": "/plant-clicky-game/static/js/runtime-main.448e202f.js"
+  }
+]);
